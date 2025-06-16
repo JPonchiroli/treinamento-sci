ac7c8a9a21238c382940e6a7de7546fc31e7cdbd
jponchiroli-sci <joao.pedro@sci.com.br>
2025-06-16 17:57:05 -0300

    adicionando treinamento 16-06-2025

--- php/joaop/academiaIndex.php
+++ php/joaop/academiaIndex.php
@@ -16,10 +16,92 @@
     </header>
 
     <main>
-        <section>
+        <section class="containerBotao">
             <button class="botaoCadastrar">
                 <a href="academiaForm.php">Cadastro</a>
             </button>
+
+            <menu>
+                <button class="botaoCadastrar" id="modal">Cadastro Modal</button>
+            </menu>
+
+            <dialog close class="modalCadastro" id="dialog">
+
+                <?php
+                require_once("../../connectionSCI/connection.php");
+
+                $nome = $idade = $email = $celular = $sexo = "";
+                $idPessoa = "";
+
+                if (isset($_GET['idPessoa'])) {
+                    $idPessoa = mysql_real_escape_string($_GET['idPessoa']);
+                    $sql = "SELECT * FROM joaop WHERE idPessoa = '$idPessoa'";
+                    $rs = mysql_query($sql, $conexao);
+
+                    if (mysql_num_rows($rs) > 0) {
+                        $row = mysql_fetch_assoc($rs);
+                        $nome = $row['nomePessoa'];
+                        $idade = $row['idadePessoa'];
+                        $email = $row['emailPessoa'];
+                        $celular = $row['celularPessoa'];
+                        $sexo = $row['sexoPessoa'];
+                    }
+                }
+                ?>
+
+                <div class="containerTitulo">
+                    <? $funcaoTela = $idPessoa != 0 ? "Atualização de Pessoa" : "Cadastro de Pessoa" ?>
+                    <h2 class="tituloForm"> <?= $funcaoTela ?> </h2>
+                </div>
+
+                <form method="post" action="academiaFormDo.php">
+
+                    <div class="containerInputs">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" placeholder="Informe seu nome" value="<?= $nome ?>" required>
+                    </div>
+
+                    <div class="containerInputs">
+                        <label for="idade">Idade</label>
+                        <input type="text" name="idade" placeholder="Informe sua idade" value="<?= $idade ?>">
+                    </div>
+
+                    <div class="containerInputs">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" placeholder="Informe seu e-mail" value="<?= $email ?>">
+                    </div>
+
+                    <div class="containerInputs">
+                        <label for="celular">Celular</label>
+                        <input type="tel" name="celular" placeholder="Informe seu celular" value="<?= $celular ?>">
+                    </div>
+
+                    <div class="containerInputs">
+                        <label>Sexo</label>
+
+                        <div class="containerRadios">
+                            <input type="radio" name="sexo" id="sexoM" value="0" <?= $sexo === "0" ? "checked" : "" ?> placeholder="Masculino">
+                            <label class="labelSexo" for="sexoM">Masculino</label>
+
+                            <input type="radio" name="sexo" id="sexoF" value="1" <?= $sexo === "1" ? "checked" : "" ?> placeholder="Feminino">
+                            <label class="labelSexo" for="sexoF">Feminino</label>
+                        </div>
+                    </div>
+
+                    <div class="containerInputs">
+                        <input type="text" name="idPessoa" placeholder="Informe seu id" value="<?= $idPessoa ?>" style="visibility: hidden;" />
+                    </div>
+
+                    <? $funcaoBotao = $idPessoa != 0 ? "Atualizar" : "Cadastrar" ?>
+
+                    <button type="submit" class="botaoCadastrarModal"> <?= $funcaoBotao ?> </button>
+
+                </form>
+
+                <menu>
+                    <button id="cancel" type="reset">Cancel</button>
+                </menu>
+            </dialog>
         </section>
 
         <section class="containerTabela">
@@ -85,6 +167,20 @@
     </main>
 
     <script>
+        (function() {
+            var updateButton = document.getElementById("modal");
+            var cancelButton = document.getElementById("cancel");
+            var favDialog = document.getElementById("dialog");
+
+            updateButton.addEventListener("click", function() {
+                favDialog.showModal();
+            });
+
+            cancelButton.addEventListener("click", function() {
+                favDialog.close();
+            });
+        })();
+
         document.getElementById('inputBusca').addEventListener('keydown', function() {
             const nome = this.value;
 
